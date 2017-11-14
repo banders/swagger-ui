164a3ae9d84d7de90aed2005bfc938888877a02a
Brock Anderson <brock@bandersgeo.ca>
2017-11-14 09:59:46 -0800

    changed text on 'Enter API Key' button.  increased button width

--- src/core/components/auth/authorize-btn.jsx
+++ src/core/components/auth/authorize-btn.jsx
@@ -23,7 +23,7 @@ export default class AuthorizeBtn extends React.Component {
     return (
       <div className="auth-wrapper">
         <button className={isAuthorized ? "btn authorize locked" : "btn authorize unlocked"} onClick={ this.onClick }>
-          <span>Enter Existing API Key</span>
+          <span>Enter API Key</span>
           <svg width="20" height="20">
             <use href={ isAuthorized ? "#locked" : "#unlocked" } xlinkHref={ isAuthorized ? "#locked" : "#unlocked" } />
           </svg>
